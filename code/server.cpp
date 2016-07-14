49470359783aacf6a6526708eb2410c4ccb5f67b
Joe Best-Rotheray <joebestrotheray@gmail.com>
2016-07-14 21:34:03 +0100

    just uploading this bit I did at the start of lunch before getting distracted!

--- code/server.cpp
+++ code/server.cpp
@@ -1,3 +1,17 @@
+#include <stdio.h>
+#include <winsock2.h>
+
 void main()
 {
+	WORD winsock_version = ( 2 << 8 ) | 2;
+	WSADATA winsock_data;
+	if( WSAStartup( winsock_version, &winsock_data ) )
+	{
+		printf( "WSAStartup failed: %d", WSAGetLastError() );
+		return;
+	}
+
+	
+
+	
 }
