be44b18dcf0198a15670f1086cb66a036b7a2114
Joe Best-Rotheray <joebestrotheray@gmail.com>
2017-02-16 20:55:13 +0000

    - updated vulkan - changed bats so path to vulkan sdk is only specified once - fixed issue with shader compilation batch file if build/data didn't exist

--- compile_shaders.bat
+++ compile_shaders.bat
@@ -1,3 +1,4 @@
-C:\VulkanSDK\1.0.21.1\Bin\glslangValidator.exe -V code\shader.vert
-C:\VulkanSDK\1.0.21.1\Bin\glslangValidator.exe -V code\shader.frag
-move *.spv build\data
+%VULKAN_SDK_PATH%\Bin\glslangValidator.exe -V code\shader.vert
+%VULKAN_SDK_PATH%\Bin\glslangValidator.exe -V code\shader.frag
+mkdir build\data
+move *.spv build\data\
--- shell.bat
+++ shell.bat
@@ -1,2 +1,3 @@
-set INCLUDE=C:\VulkanSDK\1.0.37.0\Include;%INCLUDE%
+set VULKAN_SDK_PATH=C:\VulkanSDK\1.0.39.1
+set INCLUDE=%VULKAN_SDK_PATH%\Include;%INCLUDE%
 "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64
