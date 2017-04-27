f169d5889b303289e8e8afa211b90ee030c64586
Joe Best-Rotheray <joebestrotheray@gmail.com>
2017-04-27 21:46:55 +0100

    updated vulkan sdk

--- shell.bat
+++ shell.bat
@@ -1,3 +1,3 @@
-set VULKAN_SDK_PATH=C:\VulkanSDK\1.0.39.1
+set VULKAN_SDK_PATH=C:\VulkanSDK\1.0.46.0
 set INCLUDE=%VULKAN_SDK_PATH%\Include;%INCLUDE%
 "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64
