4a5fc1f354865f9c9195b3a946b1a86863d42589
Joe Best-Rotheray <joebestrotheray@gmail.com>
2017-06-27 23:09:03 +0100

    updated visual studio

--- shell.bat
+++ shell.bat
@@ -1,3 +1,3 @@
 set VULKAN_SDK_PATH=C:\VulkanSDK\1.0.51.0
 set INCLUDE=%VULKAN_SDK_PATH%\Include;%INCLUDE%
-"C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64
+"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
